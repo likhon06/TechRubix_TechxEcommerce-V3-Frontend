c461808070d08d5170c5db725c78e0a8fef2ab35
likhon15 <likhon15-3916@diu.edu.bd>
2024-06-19 02:20:51 +0600

    added

--- src/app/(withDashboardLayout)/dashboard/user/layout.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/layout.tsx
@@ -1,11 +1,9 @@
 import { DDrawer } from "@/Components/UI/DDrawer/DDrawer";
 import { Toaster } from "sonner";
-import NextTopLoader from 'nextjs-toploader';
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="">
             <Toaster/>
-            <NextTopLoader/>
             <div className="grid grid-cols-1 ms-20">
                 <DDrawer />
                 <div className="me-[100px] ">
